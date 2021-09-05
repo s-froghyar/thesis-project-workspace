f63a4b96bc4713d1cb8a4dc0b81f541e717021d1
TheSoma300 <froghyar.soma@gmal.com>
2021-09-05 20:44:06 +0100

    removing nestjs from workspace

--- apps/thesis/src/app/app.component.ts
+++ apps/thesis/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@somaf-ws/api-interfaces';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './animations';
 
@@ -11,7 +10,6 @@ import { slideInAnimation } from './animations';
   animations: [slideInAnimation]
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
   constructor(private http: HttpClient) {}
   prepareRoute(outlet: RouterOutlet){
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
